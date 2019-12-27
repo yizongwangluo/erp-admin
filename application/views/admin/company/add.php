339e59a508913fa77cddc5f9cd51962126bda643
liuxiaojie_sz@163.com <liuxiaojie_sz@163.com>
2019-12-27 11:23:19 +0800

    文件同步

--- application/views/admin/company/add.php
+++ application/views/admin/company/add.php
@@ -1,130 +1,131 @@
 <?php $this->load->view ( 'admin/common/header' ) ?>
 <?php $this->load->view ( 'admin/common/menu' ) ?>
-    <div class="layui-tab admin-layui-tab layui-tab-brief">
-        <ul class="layui-tab-title">
-            <li class=""><a href='<?php echo base_url ( 'admin/company/index' ) ?>'>企业主体列表</a></li>
-            <li class="layui-this">新增企业主体</li>
-        </ul>
-        <div class="layui-tab-content">
-            <div class="layui-tab-item layui-show"  >
-<form action="<?php echo base_url ( 'admin/company/add' ) ?>" class="layui-form" method="post">
-    <input type="hidden" value="<?=$info['id']?>"  name="id">
-    <div class="layui-form-item">
-        <div class="layui-inline">
-            <label class="layui-form-label">*代理商</label>
-            <div class="layui-input-inline">
-                <input type="text" name="agent" value="<?=$info['agent']?>" placeholder="" class="layui-input">
-            </div>
-        </div>
-        <div class="layui-inline">
-            <label class="layui-form-label">*公司名称</label>
-                <div class="layui-input-inline">
-                    <input type="text" name="company_name" value="<?=$info['company_name']?>" placeholder="" class="layui-input">
+<div class="layui-tab admin-layui-tab layui-tab-brief">
+    <ul class="layui-tab-title">
+        <li class=""><a href='<?php echo base_url ( 'admin/company/index' ) ?>'>企业主体列表</a></li>
+        <li class="layui-this">新增企业主体</li>
+    </ul>
+    <div class="layui-tab-content">
+        <div class="layui-tab-item layui-show"  >
+            <form action="<?php echo base_url ( 'admin/company/add' ) ?>" class="layui-form" method="post">
+                <input type="hidden" value="<?=$info['id']?>"  name="id">
+                <div class="layui-form-item">
+                    <div class="layui-inline">
+                        <label class="layui-form-label">*代理商</label>
+                        <div class="layui-input-inline">
+                            <input type="text" name="agent" value="<?=$info['agent']?>" placeholder="" class="layui-input">
+                        </div>
+                    </div>
+                    <div class="layui-inline">
+                        <label class="layui-form-label">*公司名称</label>
+                        <div class="layui-input-inline">
+                            <input type="text" name="company_name" value="<?=$info['company_name']?>" placeholder="" class="layui-input">
+                        </div>
+                    </div>
                 </div>
+                <div class="layui-form-item">
+                    <label class="layui-form-label">*营业执照</label>
+                    <div class="layui-inline">
+                        <img src="<?php echo $info['business_license_image'] ? : '';?> "  class="thumb_img" style="width: 150px;height: 150px"/>
+                        <input id="thumb_img" name="business_license_image" value="<?=$info['business_license_image']?>" type="hidden" class="layui-input thumb_img" />
+                    </div>
+                    <div class="layui-inline">
+                        <a id="thumb_img_btn"  href="javascript:void(0)" class="layui-btn upload-img-all" >上传图片</a>
+                    </div>
+                </div>
+                <div class="layui-form-item">
+                    <div class="layui-inline">
+                        <label class="layui-form-label">*广告主联系人姓名</label>
+                        <div class="layui-input-inline">
+                            <input type="text" name="ad_connect_name" value="<?=$info['ad_connect_name']?>" placeholder="" class="layui-input">
+                        </div>
+                    </div>
+                    <div class="layui-inline">
+                        <label class="layui-form-label">*广告主联系人邮箱</label>
+                        <div class="layui-input-inline">
+                            <input type="text" name="ad_connect_email" value="<?=$info['ad_connect_email']?>" placeholder="" class="layui-input">
+                        </div>
+                    </div>
+                </div>
+                <div class="layui-form-item">
+                    <div class="layui-inline">
+                        <label class="layui-form-label">FB粉丝页链接</label>
+                        <div class="layui-input-inline">
+                            <input type="text" name="fanslink" value="<?=$info['fanslink']?>" placeholder="" class="layui-input">
+                            <em>多个链接以 , 隔开</em>
+                        </div>
+                    </div>
+                    <div class="layui-inline">
+                        <label class="layui-form-label">时区</label>
+                        <div class="layui-input-inline">
+                            <input type="text" name="time_zone" value="<?=$info['time_zone']?>" placeholder="" class="layui-input">
+                        </div>
+                    </div>
+                </div>
+                <div class="layui-form-item">
+                    <div class="layui-inline">
+                        <label class="layui-form-label">BM</label>
+                        <div class="layui-input-inline">
+                            <input type="text" name="BM" value="<?=$info['BM']?>" placeholder="" class="layui-input">
+                        </div>
+                    </div>
+                    <div class="layui-inline">
+                        <label class="layui-form-label">开户状态</label>
+                        <div class="layui-input-inline">
+                            <select name="account_status" lay-filter="account_status">
+                                <option value="">请选择</option>
+                                <option value="1" <?php if($info['account_status'] == "1"){echo "selected=\"selected\"";} ?>>审核中</option>
+                                <option value="0" <?php if($info['account_status'] == "0"){echo "selected=\"selected\"";} ?>>审核成功</option>
+                                <option value="2" <?php if($info['account_status'] == "2"){echo "selected=\"selected\"";} ?>>审核失败</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="layui-form-item">
+                    <div class="layui-inline">
+                        <label class="layui-form-label">下户时间</label>
+                        <div class="layui-input-inline">
+                            <input type="text" class="layui-input" id="date" placeholder="请选择日期" name="logout_time" value="<?= $info['logout_time'] ? date('Y-m-d',$info['logout_time']) : '' ?>">
+                        </div>
+                    </div>
+                    <div class="layui-inline">
+                        <label class="layui-form-label">BM API</label>
+                        <div class="layui-input-inline">
+                            <input type="text" name="BMAPI" value="<?=$info['BMAPI']?>" placeholder="" class="layui-input">
+                        </div>
+                    </div>
+                </div>
+                <div class="layui-form-item">
+                    <div class="layui-inline">
+                        <label class="layui-form-label">所属人</label>
+                        <div class="layui-input-inline">
+                            <select name="belong_to" lay-search="">
+                                <option value="">直接选择或搜索选择</option>
+                                <?php foreach ($users as $v): ?>
+                                    <option value="<?=$v['s_u_id']?>" <?php if ( $info['belong_to'] == $v['s_u_id'] ){echo "selected=\"selected\"";}?>><?=$v['s_user_name']?></option>
+                                <?php endforeach;?>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="layui-inline">
+                        <label class="layui-form-label">备注</label>
+                        <div class="layui-input-inline">
+                            <input type="text" name="company_remark" value="<?=$info['company_remark']?>" placeholder="" class="layui-input">
+                        </div>
+                    </div>
+                </div>
+                <div class="layui-form-item" style="text-align: center;width: 50%;">
+                    <div class="layui-inline">
+                        <button type="button" class="layui-btn" data-url="<?php echo base_url ( 'admin/company/index' ) ?>" lay-submit lay-filter="post">保存</button>
+                    </div>
+                    <div class="layui-inline">
+                        <a href='<?php echo base_url ( 'admin/company/index' ) ?>'><button type="button" class="layui-btn ">取消</button></a>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
-    <div class="layui-form-item">
-        <label class="layui-form-label">*营业执照</label>
-        <div class="layui-inline">
-            <input id="thumb_img" name="business_license_image" value="<?=$info['business_license_image']?>" type="text" class="layui-input thumb_img" />
-        </div>
-        <div class="layui-inline">
-            <a id="thumb_img_btn"  href="javascript:void(0)" class="layui-btn upload-img-all" >上传图片</a>
-        </div>
-    </div>
-    <div class="layui-form-item">
-        <div class="layui-inline">
-            <label class="layui-form-label">*广告主联系人姓名</label>
-            <div class="layui-input-inline">
-                <input type="text" name="ad_connect_name" value="<?=$info['ad_connect_name']?>" placeholder="" class="layui-input">
-            </div>
-        </div>
-        <div class="layui-inline">
-            <label class="layui-form-label">*广告主联系人邮箱</label>
-            <div class="layui-input-inline">
-                <input type="text" name="ad_connect_email" value="<?=$info['ad_connect_email']?>" placeholder="" class="layui-input">
-            </div>
-        </div>
-    </div>
-    <div class="layui-form-item">
-        <div class="layui-inline">
-            <label class="layui-form-label">FB粉丝页链接</label>
-            <div class="layui-input-inline">
-                <input type="text" name="fanslink" value="<?=$info['fanslink']?>" placeholder="" class="layui-input">
-                <em>多个链接以 , 隔开</em>
-            </div>
-        </div>
-        <div class="layui-inline">
-            <label class="layui-form-label">时区</label>
-            <div class="layui-input-inline">
-                <input type="text" name="time_zone" value="<?=$info['time_zone']?>" placeholder="" class="layui-input">
-            </div>
-        </div>
-    </div>
-    <div class="layui-form-item">
-        <div class="layui-inline">
-            <label class="layui-form-label">BM</label>
-            <div class="layui-input-inline">
-                <input type="text" name="BM" value="<?=$info['BM']?>" placeholder="" class="layui-input">
-            </div>
-        </div>
-        <div class="layui-inline">
-            <label class="layui-form-label">开户状态</label>
-            <div class="layui-input-inline">
-                <select name="account_status" lay-filter="account_status">
-                    <option value="">请选择</option>
-                    <option value="1" <?php if($info['account_status'] == "1"){echo "selected=\"selected\"";} ?>>审核中</option>
-                    <option value="0" <?php if($info['account_status'] == "0"){echo "selected=\"selected\"";} ?>>审核成功</option>
-                    <option value="2" <?php if($info['account_status'] == "2"){echo "selected=\"selected\"";} ?>>审核失败</option>
-                </select>
-            </div>
-        </div>
-    </div>
-    <div class="layui-form-item">
-        <div class="layui-inline">
-            <label class="layui-form-label">下户时间</label>
-            <div class="layui-input-inline">
-                <input type="text" class="layui-input" id="date" placeholder="请选择日期" name="logout_time" value="<?= $info['logout_time'] ? date('Y-m-d',$info['logout_time']) : '' ?>">
-            </div>
-        </div>
-        <div class="layui-inline">
-            <label class="layui-form-label">BM API</label>
-            <div class="layui-input-inline">
-                <input type="text" name="BMAPI" value="<?=$info['BMAPI']?>" placeholder="" class="layui-input">
-            </div>
-        </div>
-    </div>
-    <div class="layui-form-item">
-        <div class="layui-inline">
-            <label class="layui-form-label">所属人</label>
-            <div class="layui-input-inline">
-                <select name="belong_to" lay-search="">
-                    <option value="">直接选择或搜索选择</option>
-                    <?php foreach ($users as $v): ?>
-                        <option value="<?=$v['s_u_id']?>" <?php if ( $info['belong_to'] == $v['s_u_id'] ){echo "selected=\"selected\"";}?>><?=$v['s_user_name']?></option>
-                    <?php endforeach;?>
-                </select>
-            </div>
-        </div>
-        <div class="layui-inline">
-            <label class="layui-form-label">备注</label>
-            <div class="layui-input-inline">
-                <input type="text" name="company_remark" value="<?=$info['company_remark']?>" placeholder="" class="layui-input">
-            </div>
-        </div>
-    </div>
-    <div class="layui-form-item" style="text-align: center;width: 50%;">
-        <div class="layui-inline">
-            <button type="button" class="layui-btn" data-url="<?php echo base_url ( 'admin/company/index' ) ?>" lay-submit lay-filter="post">保存</button>
-        </div>
-        <div class="layui-inline">
-            <a href='<?php echo base_url ( 'admin/company/index' ) ?>'><button type="button" class="layui-btn ">取消</button></a>
-        </div>
-    </div>
-</form>
-            </div>
-        </div>
-    </div>
+</div>
 <?php $this->load->view ( 'admin/common/footer' ) ?>
 
 <script>
---
+++ application/views/admin/goods/index.php
@@ -0,0 +1,63 @@
+<?php $this->load->view ( 'admin/common/header' ) ?>
+<?php $this->load->view ( 'admin/common/menu' ) ?>
+<style>
+    .img{
+        /*width: 10%;word-wrap:break-word;word-break:break-all;*/
+    }
+</style>
+<div class="layui-tab admin-layui-tab layui-tab-brief">
+    <ul class="layui-tab-title">
+        <li class="layui-this">商品列表</li>
+        <li><a href="<?php echo base_url ( 'admin/goods/add' ) ?>">导入</a></li>
+    </ul>
+    <div class="layui-tab-content">
+            <form action="?" method="get">
+                <div class="layui-form">
+                    <div class="layui-inline  col-xs-3">
+                        <input type="text" name="keyword" value="<?=$where['keyword']?>"
+                               class="layui-input" placeholder="输入关键词"/>
+                    </div>
+                    <button class="layui-btn layui-btn-danger btn-search" type="submit">搜索
+                </div>
+            </form>
+            <table class="layui-table">
+              <thead>
+                <tr>
+                    <td>ID</td>
+                    <td class="img">产品图片</td>
+                    <td>产品名</td>
+                    <td>SKU</td>
+                    <td>规格</td>
+                    <td>采购价（元）</td>
+                    <td>重量（克）</td>
+                    <td>同步（通途）</td>
+                    <td>操作</td>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($data as $v): ?>
+                  <tr>
+                      <td><?=$v['id']?></td>
+                      <td class="img"><a href="<?=base_url($v['img'])?>" target="_blank"><img src="<?=base_url($v['img'])?>"></a></td>
+                      <td><?=$v['name']?></td>
+                      <td><?=$v['sku_code']?></td>
+                      <td><?=$v['norms']?></td>
+                      <td><?=$v['price']?></td>
+                      <td><?=$v['weight']?></td>
+                      <td style="color: <?=$v['is_tongtu']!=1?'red':'';?>"><?=$v['is_tongtu']?'已同步':'未同步'?></td>
+                      <td>
+                          <a class="layui-btn layui-btn-xs" href="<?=base_url("admin/goods/info/{$v['id']}"); ?>">查看</a>
+                          <button data-url="<?php echo base_url ( 'admin/goods/synchronization' ) ?>" data-id="<?= $v['id'] ?>" class="layui-btn layui-btn-xs confirm_post layui-btn-warm">同步到通途</button>
+                         <!-- <button style="display: <?/*=$v['status']==1?'none':'';*/?>;" data-url="<?php /*echo base_url ( 'admin/goods/delete' ) */?>" data-id="<?/*= $v['id'] */?>" class="layui-btn layui-btn-xs layui-btn-danger confirm_post">删除</button>-->
+                      </td>
+                  </tr>
+                <?php endforeach;?>
+                </tbody>
+            </table>
+        <div class="admin-page">
+            <?php echo $page_html; ?>
+        </div>
+    </div>
+</div>
+
+<?php $this->load->view ( 'admin/common/footer' ) ?>
