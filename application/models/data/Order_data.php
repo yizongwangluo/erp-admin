5eb3ac2136ae049e6fb743a6e2d96b875313031e
BecauseSo <liuxiaojie_sz@163.com>
2020-05-21 15:19:41 +0800

    同步订单 data bug修复

--- application/models/data/Order_data.php
+++ application/models/data/Order_data.php
@@ -21,7 +21,7 @@ class Order_data extends \Application\Component\Common\IData{
         }else{ //存在则修改  并记录日志
             unset($info['datetime']); //不修改时间
             unset($info['addtime']); //不修改时间
-            $re = $this->update($info);
+            $re = $this->update($order_info['id'],$info);
             if(!$re){
                 log_message('order_edit_err',date('Y-m-d H:i:s').'-------id='.$order_info['id'].'-------'.json_encode($info).'------',true);
             }else{
@@ -30,59 +30,6 @@ class Order_data extends \Application\Component\Common\IData{
         }
         return false;
     }
-
-
-    /**
-     * 添加
-     * @param int $shop_id
-     * @param string $time
-     * @param array $order_list
-     * @return bool
-     */
-    public function add_order($shop_id = 0,$time = '',$order_list = []){
-        try {
-            $this->db->trans_strict(FALSE);
-            $this->db->trans_begin();
-
-            $order = [];
-            $order_goods = [];
-
-            $order_sql = 'insert into `order` (shopify_o_id,shop_id,total_price_usd,created_at,updated_at,total_weight,financial_status,datetime) values ';
-            $order_goods_sql = 'insert into order_goods (product_id,sku_id,shop_id,shopify_o_id,quantity,datetime) values ';
-
-            foreach($order_list as $k=>$value){
-                $date = substr($value['updated_at'],0,strpos($value['updated_at'], 'T'));
-                $order[] = '("'.$value['id'].'",'.$shop_id.','.$value['total_price_usd'].',"'.$value['created_at'].'","'.$value['updated_at'].'",'.$value['total_weight'].',"'.$value['financial_status'].'","'.$date.'")';
-
-                foreach($value['line_items'] as $i=>$item){
-                    $order_goods[] = '("'.$item['product_id'].'","'.$item['sku'].'",'.$shop_id.',"'.$value['id'].'",'.$item['quantity'].',"'.$date.'")';
-                }
-            }
-
-            $order_sql .= implode(',',$order);
-            $query = $this->db->query($order_sql);
-            if($this->db->affected_rows()<=0){ //添加失败
-                log_message('add_order','order_sql-'.$time.':'.$order_sql,true);
-                return false;
-            }
-
-            $order_goods_sql .= implode(',',$order_goods);
-            $query = $this->db->query($order_goods_sql);
-            if($this->db->affected_rows()<=0){ //添加失败
-                log_message('add_order','order_goods_sql-'.$time.':'.$order_goods_sql,true);
-                return false;
-            }
-
-            $this->db->trans_complete();
-            return true;
-
-        }catch(PDOException $e) {
-            $this->db->trans_rollback();
-            log_message('add_order','add_order-'.$time.':'.$e->getMessage(),true);
-//            exit($e->getMessage());
-        }
-    }
-
 
     /**
      * 绑定运费
